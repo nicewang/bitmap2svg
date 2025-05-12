fc52559d318e6d974ee3c3faf97a3b578a82109d
nicewang <15051965381@163.com>
2025-05-12 13:03:33 +0800

    cmt-20250511-2

--- setup.py
+++ setup.py
@@ -1,27 +1,49 @@
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
-import pybind11
+from setuptools import setup, find_packages, Extension
+import os
+import sys
 
-ext_modules = [
-    Extension(
-        'bitmap2svg_core',
-        ['bitmap2svg/cpp/bitmap_to_svg.cpp', 'bitmap2svg/cpp/bindings.cpp'],
-        include_dirs=[
-            pybind11.get_include(),
-            pybind11.get_include(user=True),
-        ],
-        libraries=["potrace"],
-        language='c++',
-        extra_compile_args=['-std=c++11', '-stdlib=libc++'],
-    ),
-]
+try:
+    import pybind11
+except ImportError:
+    raise RuntimeError("Pybind11 is required. Please install it with 'pip install pybind11'")
+
+tracer_module = Extension(
+    'bitmap2svg._tracer',
+    sources=[
+        'bitmap2svg/cpp/bitmap_to_svg.cpp',
+        'bitmap2svg/cpp/bindings.cpp',
+    ],
+    libraries=['potrace'],
+    include_dirs=[
+        'bitmap2svg/cpp',
+        pybind11.get_include(),
+        pybind11.get_include(user=True),
+    ],
+    extra_compile_args=['-std=c++11', '-fPIC'],
+    extra_link_args=[],
+    language='c++'
+)
 
 setup(
     name='bitmap2svg',
     version='0.2.0',
+    packages=find_packages(),
+    ext_modules=[tracer_module],
+    install_requires=[
+        'Pillow',
+        'numpy',
+        'pybind11>=2.6',
+    ],
     author='Xiaonan (Nice) Wang',
-    description='Convert bitmap to SVG',
-    packages=['bitmap2svg'],
-    ext_modules=ext_modules,
-    zip_safe=False,
+    description='Convert bitmap images to SVG',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Opensource-Nice-Arishi/kaggle_drawing_with_LLMs/tree/bitmap2svg',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Multimedia :: Graphics :: Convert',
+        'Topic :: Scientific/Engineering :: Image Processing',
+    ],
 )
