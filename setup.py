59a723dc58a950194391e4cedca21f6e51cfdb83
nicewang <15051965381@163.com>
2025-05-12 16:48:10 +0800

    cmt-20250511-23

--- setup.py
+++ setup.py
@@ -1,44 +1,33 @@
-from setuptools import setup, find_packages
-from pybind11.setup_helpers import Pybind11Extension, build_ext
-import os
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import pybind11
+import sys
 
-cpp_sources = [
-    'bitmap2svg/cpp/bitmap_to_svg.cpp',
-    'bitmap2svg/cpp/bindings.cpp',
+class get_pybind_include(object):
+    def __str__(self):
+        return pybind11.get_include()
+
+ext_modules = [
+    Extension(
+        "_bitmap2svg_core",
+        sources=[
+            "bitmap2svg/cpp/bindings.cpp",
+            "bitmap2svg/cpp/bitmap_to_svg.cpp",
+        ],
+        include_dirs=[
+            "bitmap2svg/cpp",
+            get_pybind_include(),
+        ],
+        language="c++",
+        extra_compile_args=["-std=c++17"]
+    ),
 ]
-
-def get_ext_modules():
-    import pybind11
-    return [
-        Pybind11Extension(
-            "_bitmap2svg_core",
-            sources=cpp_sources,
-            include_dirs=[
-                'bitmap2svg/cpp',
-                pybind11.get_include(),
-            ],
-            libraries=["potrace"],
-            extra_compile_args=["-std=c++17"],
-        )
-    ]
 
 setup(
     name="bitmap2svg_potrace",
     version="0.2.0",
-    author="Xiaonan (Nice) Wang",
-    author_email="wangxiaonannice@gmail.com",
-    description="A Python wrapper for bitmap to SVG conversion using Potrace and pybind11",
-    long_description=open('README.md').read() if os.path.exists('README.md') else '',
-    long_description_content_type="text/markdown",
-    url="https://github.com/Opensource-Nice-Arishi/kaggle_drawing_with_LLMs/tree/bitmap2svg",
-    packages=find_packages(where='.'),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.8',
-    ext_modules=get_ext_modules(),
+    packages=["bitmap2svg"],
+    ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
 )
