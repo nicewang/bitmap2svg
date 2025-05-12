9116145f4d3a3d75d20755324b5764412d95a988
nicewang <15051965381@163.com>
2025-05-12 18:35:16 +0800

    cmt-20250511-29

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
 from setuptools import setup
-setup()
+
+setup(
+    name="bitmap2svg",
+    version="0.2.0",
+    packages=["bitmap2svg"],
+    package_data={
+        "bitmap2svg": ["CMakeLists.txt"],
+    },
+    include_package_data=True,
+)
