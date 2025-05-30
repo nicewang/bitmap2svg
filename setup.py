7bf8d26ac45e67cfa8014c0da8d04378a8187df7
nicewang <15051965381@163.com>
2025-05-30 15:22:59 +0800

    cmt-20250529-6

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="bitmap2svg-cu",
-    version="0.2.3",
+    name="bitmap2svg",
+    version="0.2.3.1",
     description="A library to convert bitmaps to SVG, using C++ and OpenCV.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -10,5 +10,5 @@ setup(
     author_email="wangxiaonannice@gmail.com",
     license="MIT",  
     url="https://github.com/Opensource-Nice-Arishi/kaggle_drawing_with_LLMs/tree/bitmap2svg-cu-v0.2.3",
-    packages=["bitmap2svg-cu"],
+    packages=["bitmap2svg"],
 )
