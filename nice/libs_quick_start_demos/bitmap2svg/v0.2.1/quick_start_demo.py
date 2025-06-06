4e2023691e782b557572a45840941beecc3fe04c
nicewang <15051965381@163.com>
2025-06-06 16:22:09 +0800

    cmt-20250606-2

--- nice/libs_quick_start_demos/bitmap2svg/v0.2.1/quick_start_demo.py
+++ nice/libs_quick_start_demos/bitmap2svg/v0.2.1/quick_start_demo.py
@@ -2,13 +2,13 @@ from PIL import Image
 from bitmap2svg import bitmap_to_svg
 
 try:
-    img = Image.open("0013.png")
+    img = Image.open("WechatIMG31.jpg")
 
     # Convert to SVG
     svg_output = bitmap_to_svg(img, num_colors=None)
 
     # Save the SVG to a file
-    with open("output.svg", "w") as f:
+    with open("output_ocean.svg", "w") as f:
         f.write(svg_output)
 
     print("SVG generated successfully!")
